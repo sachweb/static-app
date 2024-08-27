e7d5b515a9460b3fea2b2cccfa93420eedb10879
sachweb <ssmewada1423@gmail.com>
2024-08-27 14:39:43 +0530

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+     output: 'export',
+     basePath: '/static-spp',
+     assetPrefix: '/static-spp/',
+};
+
+export default nextConfig;
