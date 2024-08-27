76671bf6fdb12f4d1a54b8b5687a4166b4c7725b
sachweb <ssmewada1423@gmail.com>
2024-08-27 16:10:56 +0530

    new Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  });
 const nextConfig = {
+    compress: true,
     output: 'export',
     assetPrefix: isProd ? '/static-app/' : '',
     basePath: isProd ? '/static-app' : '',
